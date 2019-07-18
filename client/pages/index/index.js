0ffcfef10e09b054ce6b1f0b4732da9d5f781e5e
Felix <13115412392@163.com>
2019-07-18 19:12:37 +0800

    轮播图

---
+++ client/pages/index/index.js
@@ -0,0 +1,74 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'https://lc-imageresizer-live-s.legocdn.com/resize/explore_assistance?width=720&imageUrl=https%3a%2f%2fwww.lego.com%2fr%2fwww%2fr%2fportals%2f-%2fmedia%2flegocom%2fhome%2fexplore%2fexplore_assistance.jpg%3fl.r%3d404717280',
+      'https://lc-imageresizer-live-s.legocdn.com/resize/explore_app-kids?width=720&imageUrl=https%3a%2f%2fwww.lego.com%2fr%2fwww%2fr%2fportals%2f-%2fmedia%2flegocom%2fhome%2fexplore%2fexplore_app-kids.jpg%3fl.r%3d-284768056',
+      'https://lc-imageresizer-live-s.legocdn.com/resize/explore_cars?width=720&imageUrl=https%3a%2f%2fwww.lego.com%2fr%2fwww%2fr%2fportals%2f-%2fmedia%2flegocom%2fhome%2fexplore%2fexplore_cars.jpg%3fl.r%3d1822749250'
+    ],
+    indicatorDots: true, // 是否显示面板指示点
+    autoplay: true,      // 是否自动切换
+    circular: true,      // 是否采用衔接滑动
+    interval: 3000,      // 自动切换时间间隔
+    duration: 1000,      // 滑动动画时长
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
